feb12d512d58dc2f15aef08d1b325cdec95747b3
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-10-28 15:31:33 +0530

    header and add button

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>To-Do List</h2>
+      <Button />
+    </div>
+  );
+};
+
+export default Header;
