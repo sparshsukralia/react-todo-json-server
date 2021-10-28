f0ee0148c527279d99e55fba1a8bca5d40638970
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-10-28 19:53:46 +0530

    json server added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,48 @@ import Items from "./components/Items";
 function App() {
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    const getItems = async () => {
+      const itemsFromServer = await fetchItems();
+      setItems(itemsFromServer);
+    };
+    getItems();
+  }, []);
+
+  // Fetch items
+  const fetchItems = async () => {
+    const res = await fetch("http://localhost:5000/items");
+    const data = await res.json();
+    return data;
+  };
+
   // Add an item
-  const addItem = (item) => {
-    // creating a new id for the new item to add
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newItem = { id, ...item };
-    // adding the new item to the list
-    setItems([...items, newItem]);
+  const addItem = async (item) => {
+    const res = await fetch(`http://localhost:5000/items`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+
+    const data = await res.json();
+
+    setItems([...items, data]);
+
+    // // creating a new id for the new item to add
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newItem = { id, ...item };
+    // // adding the new item to the list
+    // setItems([...items, newItem]);
   };
 
   // Delete an item
-  const deleteItem = (id) => {
+  const deleteItem = async (id) => {
+    await fetch(`http://localhost:5000/items/${id}`, {
+      method: "DELETE",
+    });
+
     setItems(items.filter((item) => item.id !== id));
   };
 
